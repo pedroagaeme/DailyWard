dfe55e1848c9439dc98ff49fd084ee7bd0d91545
pedroagaeme <futurograduado@gmail.com>
2025-09-25 22:25:35 +0000

    correção dos métodos POST e GET da rota de posts

--- backend/dailyward/posts/serializers.py
+++ backend/dailyward/posts/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    poster_name = serializers.CharField(source='posted_by.get_full_name', read_only=True)
     class Meta:
         model = Post
-        fields = ['id', 'topic', 'posted_by', 'content_text', 'created_at']
-        read_only_fields = ['posted_by', 'created_at', 'topic']
+        fields = ['id', 'posted_by', 'poster_name', 'content_text', 'created_at']
+        read_only_fields = ['posted_by', 'poster_name', 'created_at']
--- backend/dailyward/posts/views.py
+++ backend/dailyward/posts/views.py
@@ -35,31 +35,6 @@ class PostViewSet(viewsets.ModelViewSet):
         topic_id = self.kwargs.get('topic_pk') 
         topic = Topic.objects.get(pk=topic_id)
         post = serializer.save(posted_by=self.request.user, topic=topic)
-#create your views here.
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    
-    def get_permissions(self):
-        # map actions to small permissions
-        if self.action in ("list", "retrieve", "create"):
-            return [permissions.IsAuthenticated(), IsTopicParticipant()]
-        if self.action in ("update", "partial_update"):
-            return [permissions.IsAuthenticated(), IsAuthor()]
-        if self.action == "destroy":
-            return [permissions.IsAuthenticated(), IsAuthorOrTopicAdmin()]
-        return [permissions.IsAuthenticated()]
-    
-    def get_queryset(self):
-        # filter posts by topic from URL
-        topic_id = self.kwargs.get('topic_pk')          
-        return Post.objects.filter(topic=topic_id)
-
-    def perform_create(self, serializer):
-        topic_id = self.kwargs.get('topic_pk') 
-        topic = Topic.objects.get(pk=topic_id)
-        post = serializer.save(posted_by=self.request.user, topic=topic)
     
 class PostsByDayAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated, IsTopicParticipant]
